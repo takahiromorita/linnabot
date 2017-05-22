5fd3773cf181738ee6ea90cfab0882c877f8e736
takahiromorita <moonchild194@gmail.com>
2017-05-22 17:47:56 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,9 +10,9 @@ import falcon
 import psycopg2
 conn = psycopg2.connect("dbname=d60eumuvp125t8 host=ec2-174-129-227-116.compute-1.amazonaws.com user=rrzanzdfkiuvot password=888af4acd6219fe826b95173080870c57685f3fa912285b82dbd56d563d34fdb")
 cur = conn.cursor()
-#cur.execute("INSERT INTO contextdb (context, date) VALUES ('aaa', )")
-#cur.fetchone()
-#conn.commit()
+cur.execute("INSERT INTO contextdb (context, date) VALUES ('aaa', '2017-05-10 00:00:00')")
+cur.fetchone()
+conn.commit()
 cur.close()
 conn.close()
 
