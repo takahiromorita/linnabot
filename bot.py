8404df608ae5f8df6f008b322784365077556aaf
takahiromorita <moonchild194@gmail.com>
2017-05-22 22:48:29 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ import doco.client
 import falcon
 
 import psycopg2
+import urlparse
 #conn = psycopg2.connect("dbname=d60eumuvp125t8 host=ec2-174-129-227-116.compute-1.amazonaws.com user=rrzanzdfkiuvot password=888af4acd6219fe826b95173080870c57685f3fa912285b82dbd56d563d34fdb")
 urlparse.uses_netloc.append("postgres")
 url = urlparse.urlparse(os.environ["ec2-174-129-227-116.compute-1.amazonaws.com"])
