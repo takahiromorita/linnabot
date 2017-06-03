89185707af412cbb68239aa835f17145135ddafd
takahiromorita <moonchild194@gmail.com>
2017-06-03 14:44:53 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -69,7 +69,7 @@ class CallbackResource(object):
                     )
                     
                     if event['message']['text'].find('?') > -1:
-                        docomo_res = requests.get(DOCOMO_QA_ENDPOINT+'?q='+event['message']['text'].encode('utf-8')+'APIKEY='+DOCOMO_API_KEY)
+                        docomo_res = requests.get(DOCOMO_QA_ENDPOINT+'?q='+event['message']['text'].encode('utf-8')+'&APIKEY='+DOCOMO_API_KEY)
                     else:
                         cur = conn.cursor()
                         cur.execute("SELECT * FROM contexttb ORDER BY id DESC LIMIT 1")
