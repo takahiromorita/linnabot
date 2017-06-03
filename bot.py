497c7fe61e74cca4987dc3cf124eaf7200028f30
takahiromorita <moonchild194@gmail.com>
2017-06-03 15:49:40 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -72,12 +72,13 @@ class CallbackResource(object):
                     if event['message']['text'].find('?') > -1:
                         #logger.debug('test_content0: {}'.format(event['message']['text'].decode('utf-8').encode('utf-8')))
                         #logger.debug('test_content: {}'.format(DOCOMO_QA_ENDPOINT+'?q='+event['message']['text'].encode('utf-8')))
-                        s = requests.session()
+                        #s = requests.session()
                         params={'q':event['message']['text'], 'APIKEY':DOCOMO_API_KEY}
-                        r = s.get(DOCOMO_QA_ENDPOINT, params=params)
+                        r = requests.get(DOCOMO_QA_ENDPOINT, params=params)
                         #docomo_res = requests.get(DOCOMO_QA_ENDPOINT+'?q='+event['message']['text']+'&APIKEY='+DOCOMO_API_KEY)
-                        res_json = json.loads(r.text)
-                        logger.debug('test_aaaaa: {}'.format(res_json))
+                        docomo_res = json.loads(r.text)
+                        sys_utt = docomo_res['answers'][0]['answerText']
+                        logger.debug('test_aaaaa: {}'.format(docomo_res))
                     else:
                         cur = conn.cursor()
                         cur.execute("SELECT * FROM contexttb ORDER BY id DESC LIMIT 1")
