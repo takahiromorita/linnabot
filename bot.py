8c04ee863d082fba425956e9de622327ab3e8182
takahiromorita <moonchild194@gmail.com>
2017-06-04 12:40:54 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -110,7 +110,7 @@ class CallbackResource(object):
                             cur = conn.cursor()
                             cur.execute("SELECT * FROM tokentb ORDER BY id DESC LIMIT 1")
                             logger.debug('dialogue_testtt: {}'.format(cur.fetchone()[2]))
-                            payload = 'grant_type=refresh_token&refresh_token={}'.format(cur.fetchone()[2])
+                            #payload = 'grant_type=refresh_token&refresh_token={}'.format(cur.fetchone()[2])
                             logger.debug('dialogue_t')
                             header = {
                                 'Content-Type': 'application/x-www-form-urlencoded',
